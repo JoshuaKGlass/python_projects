cf33cbed22a67a99f8dda32a721967d2655788c6
Joshua Glass <jkglass@hotmail.co.uk>
2023-05-10 14:39:32 +0100

    parsing url info script

---
+++ A1/parse_url.py
@@ -0,0 +1,13 @@
+# this python script should parse a url and extract information from it
+
+# example string http://www.example.com/?p1=10&p2=20&p3=30
+# extract p1, p2 and p3 info
+
+
+url_string = input("Please enter a url to scan: ")
+url_string = url_string.split("?")
+url_string = url_string[1].split("&")
+
+for data in url_string:
+    data = data.split("=")
+    print("Name is " + data[0] + ", value is " + data[1])
